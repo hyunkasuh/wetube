d2a113f25d2285d5587dde91da0c32bc48e5bf06
HyunkaSuh <sgh415@gmail.com>
2018-11-21 01:01:26 -0600

    install babel, nodemon, use a little bit of ES6

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # WeTube
 
 Cloning YouTube with VanillaJS and NodeJS
+
+### Creating server with expressJS and nodeJS
+- create server
+- create routes
+- answer (send response)
+
+### babel
+- install & check the "dependencies"...
+ - @babel/node
+ - @babel/presetenv
+ - @bable/core
+- babel-node will transform the code, so that we can use the newist JS version (ES6) syntax
+ - such as arrow functions in JS
+
+ ### nodemon
+ - install, but put it in the "devDependencies" entry
+ - everytime I save the JS file, it restarts again. (No save-stop-restart)
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hi!");
+// const express = require('express');
+import express from "express";
+const app = express();
+const PORT = "4000";
+
+// function handleListening() {
+//     console.log(`Listening on: http://localhost:${PORT}`);
+// }
+// function handleHome(req, res) {
+//     // console.log(req);
+//     res.send("Hello from home!");
+// }
+// function handleProfile(req, res) {
+//     res.send("Your are on my profile page. I guess.");
+// }
+
+const handleListening = () =>
+    console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from my Home!");
+
+const handleProfile = (req, res) => res.send("Your are on my profile page. I guess.");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
